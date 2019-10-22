2143e6ce544cab91e28ae0618ffff733c5519118
Jess <Jess@sulliwood.org>
2019-10-22 08:58:48 -0400

    + Py2 version, merge setup.sh

--- client.py
+++ client.py
@@ -5,18 +5,20 @@
 #
 # systemd ideas adapted from https://derushadigital.com/other%20projects/2019/02/19/RPi-USBIP-ZWave.html
 """
+from __future__ import absolute_import
 import subprocess
 import time
+from io import open
 
-addr = '192.168.8.1'
-service_type = 'client'  # must be 'client' OR 'server'
-usbid = ''
+addr = u'192.168.8.1'
+service_type = u'client'  # must be 'client' OR 'server'
+usbid = u''
 
 
 def lsusb():
-    return str(subprocess.Popen(['lsusb'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT).stdout.read()).split('ID ')
+    return unicode(subprocess.Popen([u'lsusb'],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT).stdout.read()).split(u'ID ')
 
 
 def usb_waiter():
@@ -28,64 +30,64 @@ def usb_waiter():
 
     while len(lsusb()) <= len(start_devices):
         time.sleep(1)
-        print('waiting for new serial device...')
+        print u'waiting for new serial device...'
 
     for device in lsusb():
         new_devices.append(device[0:8])
 
-    new = str(set(new_devices) - set(start_devices)).strip("'}{ ")
-    print(str('NEW USB ID IS: ' + new))
+    new = unicode(set(new_devices) - set(start_devices)).strip(u"'}{ ")
+    print unicode(u'NEW USB ID IS: ' + new)
     return new
 
 
-""" Client Service: """
+u""" Client Service: """
 
-C_ExecStart = str('/bin/sh -c "/usr/lib/linux-tools/$(uname -r)/usbip attach -r ' +
-                  addr + ' -b $(/usr/lib/linux-tools/$(uname -r)/usbip list -r ' + addr +
-                  ' grep ' + usbid + ' | cut -d: -f1)')
+C_ExecStart = unicode(u'/bin/sh -c "/usr/lib/linux-tools/$(uname -r)/usbip attach -r ' +
+                      addr + u' -b $(/usr/lib/linux-tools/$(uname -r)/usbip list -r ' + addr +
+                      u' grep ' + usbid + u' | cut -d: -f1)')
 
-C_ExecStop = str('/bin/sh -c "/usr/lib/linux-tools/$(uname -r)/usbip detach ' +
-                 '--port=$(/usr/lib/linux-tools/$(uname -r)/usbip port ' +
-                 'grep "<Port in Use>" ' + str(' sed -E "s/^Port ([0-9][0-9]).*/\\1/'))
+C_ExecStop = unicode(u'/bin/sh -c "/usr/lib/linux-tools/$(uname -r)/usbip detach ' +
+                     u'--port=$(/usr/lib/linux-tools/$(uname -r)/usbip port ' +
+                     u'grep "<Port in Use>" ' + unicode(u' sed -E "s/^Port ([0-9][0-9]).*/\\1/'))
 
-C_contents = str('[Unit] \n' +
-                 'Description=octo_usbip \n' +
-                 'After=network.target \n\n' +
-                 '[Service] \n' +
-                 'Type=oneshot \n' +
-                 'RemainAfterExit=yes \n' +
-                 'ExecStart=' + C_ExecStart + '\n' +
-                 'ExecStop=' + C_ExecStop + '\n\n' +
-                 '[Install] \n' +
-                 'WantedBy=multi-user.target \n')
+C_contents = unicode(u'[Unit] \n' +
+                     u'Description=octo_usbip \n' +
+                     u'After=network.target \n\n' +
+                     u'[Service] \n' +
+                     u'Type=oneshot \n' +
+                     u'RemainAfterExit=yes \n' +
+                     u'ExecStart=' + C_ExecStart + u'\n' +
+                     u'ExecStop=' + C_ExecStop + u'\n\n' +
+                     u'[Install] \n' +
+                     u'WantedBy=multi-user.target \n')
 
-""" Server Service: """
+u""" Server Service: """
 
-S_ExecStart = 'ExecStart=/usr/sbin/usbipd -D'
+S_ExecStart = u'ExecStart=/usr/sbin/usbipd -D'
 
-S_ExecStartPost = str('/bin/sh -c "/usr/sbin/usbip bind --$(/usr/sbin/usbip list -p -l' +
-                      "grep '#usbid=" + usbid + "#'" + "cut '-d#' -f1)")
+S_ExecStartPost = unicode(u'/bin/sh -c "/usr/sbin/usbip bind --$(/usr/sbin/usbip list -p -l' +
+                          u"grep '#usbid=" + usbid + u"#'" + u"cut '-d#' -f1)")
 
-S_ExecStop = str('/bin/sh -c "/usr/sbin/usbip unbind --$(/usr/sbin/usbip list -p -l ' +
-                 "grep '#usbid=" + usbid + "#'" + "cut '-d#' -f1`); killall usbipd")
+S_ExecStop = unicode(u'/bin/sh -c "/usr/sbin/usbip unbind --$(/usr/sbin/usbip list -p -l ' +
+                     u"grep '#usbid=" + usbid + u"#'" + u"cut '-d#' -f1`); killall usbipd")
 
-S_contents = str('[Unit] \n' +
-                 'Description=octo_usbip \n' +
-                 'After=network.target \n\n' +
-                 '[Service] \n' +
-                 'Type=forking \n' +
-                 'RemainAfterExit=yes \n' +
-                 'ExecStart=' + S_ExecStart + '\n' +
-                 'ExecStartPost=' + S_ExecStartPost + '\n' +
+S_contents = unicode(u'[Unit] \n' +
+                     u'Description=octo_usbip \n' +
+                     u'After=network.target \n\n' +
+                     u'[Service] \n' +
+                     u'Type=forking \n' +
+                     u'RemainAfterExit=yes \n' +
+                     u'ExecStart=' + S_ExecStart + u'\n' +
+                     u'ExecStartPost=' + S_ExecStartPost + u'\n' +
 
-                 'ExecStop=' + S_ExecStop + '\n\n' +
-                 '[Install] \n' +
-                 'WantedBy=multi-user.target \n')
+                     u'ExecStop=' + S_ExecStop + u'\n\n' +
+                     u'[Install] \n' +
+                     u'WantedBy=multi-user.target \n')
 
 
 def writer():
-    f = open(str(service_type + '.service'), "w")
-    if service_type == 'client':
+    f = open(unicode(service_type + u'.service'), u"w")
+    if service_type == u'client':
         f.write(C_contents)
     else:
         f.write(S_contents)
--- server_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get install usbip
-modprobe usbip_host
-echo 'usbip_host' >> /etc/modules
-
-systemctl --system daemon-reload
-systemctl enable server.service
-systemctl start server.service
