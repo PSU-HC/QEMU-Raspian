61d23593f9c0289cc6991b0518929a83937b9c2c
Jess <Jess@sulliwood.org>
2019-10-28 07:27:17 -0400

    + Resquests

---
+++ Py3_client.py
@@ -0,0 +1,49 @@
+"""
+# Send IP address to OctoPrint server
+# WIP by Jess Sullivan
+#
+# destinations:
+# /home/pi/Py3_client.py
+# /lib/systemd/system/client.service
+"""
+import requests
+import socket
+from time import sleep
+from subprocess import Popen
+
+verbose = False  # use prints to console?  motioneye remotes are likely headless / self sufficient
+
+addr = '127.0.0.1/'  # OctoPrint Server / static address to send ip to
+port = 8080  # must match server
+
+hostname = socket.gethostname()
+myIP = socket.gethostbyname(hostname)
+
+# start usbip daemon:
+Popen('nohup usbipd &',
+      shell=True,
+      executable='/bin/bash')
+
+# bind the only usb bus on Pi Zero to usbip:
+Popen('usbip unbind -b 1-1 && sleep 1 && usbip bind -b 1-1',
+      shell=True,
+      executable='/bin/bash')
+
+
+# send ip address to server:
+
+received = bool
+
+while not received:
+
+    # POST request sent as JSON:
+    res = requests.post(str('http://' + addr + ':' + str(port) + '/'), json={"ip": myIP})
+
+    if res.status_code == 200:
+        if verbose:
+            print(str('Successfully sent IP to http server! \nMy IP is: ' + myIP))
+
+        received = True
+
+    else:
+        sleep(2)
---
+++ Py3_server.py
@@ -0,0 +1,50 @@
+"""
+# Send IP address to OctoPrint server
+# WIP by Jess Sullivan
+#
+# As of 10/28/19, OctoPrint still relies on Python2-
+# verbatim conversion from Py3:
+# ``` pip install 3to2 ```
+# ``` 3to2 -w Py3_server.py ``` # Py3 is preserved as .bak
+"""
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from optparse import OptionParser
+import json
+import subprocess
+
+verbose = False  # use prints to console?
+
+addr = '127.0.0.1/'  # server's address
+port = 8080  # must match client
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        request_headers = self.headers
+
+        # parse request (json):
+        content_length = request_headers.get('Content-Length')
+        length = int(content_length) if content_length else 0
+        remote = json.loads(self.rfile.read(length))
+
+        # mount satellite USB:
+        satellite = str('usbip attach -r ' + remote['ip'] + ' -b 1-1')
+        subprocess.Popen(satellite, shell=True, executable='/bin/bash')
+
+
+def serve(addr, port):
+    server = HTTPServer((addr, port), RequestHandler)
+    server.serve_forever()
+    parser = OptionParser()
+    (options, args) = parser.parse_args()
+
+
+# start usbip daemon:
+subprocess.Popen('nohup usbipd &',
+      shell=True,
+      executable='/bin/bash')
+
+serve(addr, port)
